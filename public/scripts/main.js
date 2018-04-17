3bb79ad0d84da129d48ded429eebd2364a027bf1
bradserlis <bradserlis@yahoo.com>
2018-04-17 10:53:35 -0700

    using index ejs with stylesheets, jquery

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -3,7 +3,7 @@ const indexRoute = express.Router();
 // const pizza = require('../db/pizza.js')
 
 indexRoute.get('/', function(req, res) {
-  res.render('index', {message: 'hello'});
+  res.render('index', {message: 'jlkjaklsjdfklajdklfjaklsdjkf'});
 });
 
 module.exports = indexRoute;
